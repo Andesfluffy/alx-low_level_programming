43c56a9a226d8f130a8c16e36597f57400f4520f
Andesfluffy <Damolaoyeyemi39@gmail.com>
2023-06-19 15:12:07 -0700

    Aded permissions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *prompt = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", prompt);
+	return (0);
+}
